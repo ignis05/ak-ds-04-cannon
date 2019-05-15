1d8ead7d79a9ed39abce53dcfec088e345c22402
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-05-15 09:40:20 +0200

    Basic scene

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,47 @@
+var scene;
+var renderer;
 $(document).ready(() => {
-    console.log('document ready');
+
+    // #region initial
+    $(window).on("resize", () => {
+        camera.aspect = $(window).width() / $(window).height()
+        camera.updateProjectionMatrix()
+        renderer.setSize($(window).width(), $(window).height())
+    })
+
+    scene = new THREE.Scene();
+
+    var camera = new THREE.PerspectiveCamera(
+        45,    // kąt patrzenia kamery (FOV - field of view)
+        $(window).width() / $(window).height(),   // proporcje widoku, powinny odpowiadać proporjom naszego ekranu przeglądarki
+        0.1,    // minimalna renderowana odległość
+        10000    // maxymalna renderowana odległość od kamery 
+    );
+    camera.position.set(300, 100, 300)
+    camera.lookAt(scene.position)
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setClearColor(0xffffff);
+    renderer.setSize($(window).width(), $(window).height());
+    //shadows
+    //renderer.shadowMap.enabled = true
+    //renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+    $("#root").append(renderer.domElement);
+    // #endregion initial
+
+    let grid = new Grid(2500)
+    grid.addTo(scene)
+
+    var orbitControl = new THREE.OrbitControls(camera, renderer.domElement);
+    orbitControl.addEventListener('change', function () {
+        renderer.render(scene, camera)
+    });
+
+
+    function render() {
+
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    } render()
 })
